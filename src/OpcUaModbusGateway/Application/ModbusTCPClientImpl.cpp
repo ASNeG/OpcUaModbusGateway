a7e8b01dff0ba3ba28ea78d25f2708d611c56bdc
Kai Huebl <kai@huebl-sgh.de>
2025-01-21 19:23:38 +0100

    write single coil implementation

--- src/OpcUaModbusGateway/Application/ModbusTCPClientImpl.cpp
+++ src/OpcUaModbusGateway/Application/ModbusTCPClientImpl.cpp
@@ -337,6 +337,7 @@ namespace OpcUaModbusGateway
 		}
 
 		// Handle response
+		errorCode = 0;
 		auto writeMultipleCoilsRes = std::static_pointer_cast<ModbusProt::WriteMultipleCoilsResPDU>(modbusRes);
 		count = writeMultipleCoilsRes->quantityOfOutputs();
 	}
@@ -387,6 +388,7 @@ namespace OpcUaModbusGateway
 		}
 
 		// Handle response
+		errorCode = 0;
 		auto writeMultipleHoldingRegistersRes = std::static_pointer_cast<ModbusProt::WriteMultipleHoldingRegistersResPDU>(modbusRes);
 		count = writeMultipleHoldingRegistersRes->quantityOfRegisters();
 	}
@@ -398,7 +400,41 @@ namespace OpcUaModbusGateway
 		uint32_t& errorCode
 	)
 	{
-		// FIXME: Test
+		ModbusProt::ModbusError modbusError;
+		ModbusProt::ModbusPDU::SPtr modbusRes;
+		Condition responseCondition;
+
+		// Create and send write single coil request
+		auto writeSingleCoilReq = std::make_shared<ModbusProt::WriteSingleCoilReqPDU>();
+		writeSingleCoilReq->address(startingAddress);
+		writeSingleCoilReq->value(value);
+		ModbusProt::ModbusPDU::SPtr req = writeSingleCoilReq;
+		modbusTCPClient_.send(0, req,
+			[this, &responseCondition, &modbusRes, &modbusError](ModbusProt::ModbusError error, ModbusProt::ModbusPDU::SPtr& req, ModbusProt::ModbusPDU::SPtr& res) {
+				modbusError = error;
+				modbusRes = res;
+				responseCondition.signal();
+			}
+		);
+
+		// Handle error
+		if (!responseCondition.wait(2000)) {
+			errorCode = static_cast<int>(ModbusProt::ModbusError::Timeout) + 100;
+			return;
+		}
+		if (modbusError != ModbusProt::ModbusError::Ok) {
+			errorCode = static_cast<int>(modbusError) + 100;
+			return;
+		}
+
+		// Handle error response
+		if (modbusRes->pduType() == ModbusProt::PDUType::Error) {
+			auto errorRes = std::static_pointer_cast<ModbusProt::ErrorPDU>(modbusRes);
+			errorCode = errorRes->exceptionCode();;
+			return;
+		}
+
+		// Handle response
 		errorCode = 0;
 	}
 
