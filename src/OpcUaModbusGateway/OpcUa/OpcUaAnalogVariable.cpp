236ec328f0ebc4fcaf77e8b77e8ef22936e7549b
Kai Huebl <kai@huebl-sgh.de>
2025-01-24 10:20:13 +0100

    add analog variable instance class

---
+++ src/OpcUaModbusGateway/OpcUa/OpcUaAnalogVariable.cpp
@@ -0,0 +1,51 @@
+/*
+   Copyright 2025 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#include <boost/make_shared.hpp>
+
+#include "OpcUaStackCore/Base/Log.h"
+
+#include "OpcUaModbusGateway/OpcUa/OpcUaAnalogVariable.h"
+
+namespace OpcUaModbusGateway
+{
+
+	OpcUaAnalogVariable::OpcUaAnalogVariable(void)
+	{
+	}
+
+	OpcUaAnalogVariable::~OpcUaAnalogVariable(void)
+	{
+	}
+
+	bool
+	OpcUaAnalogVariable::startup(
+		OpcUaStackServer::ApplicationServiceIf* applicationServiceIf
+	)
+	{
+		applicationServiceIf_ = applicationServiceIf;
+		return true;
+	}
+
+	bool
+	OpcUaAnalogVariable::shutdown(void)
+	{
+		return true;
+	}
+
+}
+
