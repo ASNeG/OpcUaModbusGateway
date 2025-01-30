3e601d392cc99bed3c31d0fe36c527c052b5d4c4
Kai Huebl <kai@huebl-sgh.de>
2025-01-30 20:01:51 +0100

    add type conversion class

---
+++ src/OpcUaModbusGateway/Util/TypeConverter.h
@@ -0,0 +1,37 @@
+/*
+   Copyright 2025 Kai Huebl (kai@huebl-sgh.de)
+
+   Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
+   Datei nur in Übereinstimmung mit der Lizenz erlaubt.
+   Eine Kopie der Lizenz erhalten Sie auf http://www.apache.org/licenses/LICENSE-2.0.
+
+   Sofern nicht gemäß geltendem Recht vorgeschrieben oder schriftlich vereinbart,
+   erfolgt die Bereitstellung der im Rahmen der Lizenz verbreiteten Software OHNE
+   GEWÄHR ODER VORBEHALTE – ganz gleich, ob ausdrücklich oder stillschweigend.
+
+   Informationen über die jeweiligen Bedingungen für Genehmigungen und Einschränkungen
+   im Rahmen der Lizenz finden Sie in der Lizenz.
+
+   Autor: Kai Huebl (kai@huebl-sgh.de)
+ */
+
+#ifndef __OpcUaModbusGateway_TypeConverter_h__
+#define __OpcUaModbusGateway_TypeConverter_h__
+
+#include <string>
+
+namespace OpcUaModbusGateway
+{
+
+	class TypeConverter
+	{
+	  public:
+		TypeConverter(void);
+		~TypeConverter(void);
+
+		static bool checkType(const std::string& typeString);
+	};
+
+}
+
+#endif
