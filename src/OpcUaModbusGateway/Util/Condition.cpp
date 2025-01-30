12336698ffd940a9085e908be4e6dcb3e1365a16
Kai Huebl <kai@huebl-sgh.de>
2025-01-30 20:00:49 +0100

    fix typo

--- src/OpcUaModbusGateway/Util/Condition.cpp
+++ src/OpcUaModbusGateway/Util/Condition.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright 2024 Kai Huebl (kai@huebl-sgh.de)
+   Copyright 2024-2025 Kai Huebl (kai@huebl-sgh.de)
 
    Lizenziert gemäß Apache Licence Version 2.0 (die „Lizenz“); Nutzung dieser
    Datei nur in Übereinstimmung mit der Lizenz erlaubt.
